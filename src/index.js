6a5b1bfd780ee53417d0534bce9a16350ffab500
Dorin <dorin.ghetus98@gmail.com>
2022-07-20 21:13:04 +0300

    C11

--- src/index.js
+++ src/index.js
@@ -1,38 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-
-
-const Header = () =>{
-  return <h2> Hello </h2>
-}
-
-const Field = () =>{
-  return <input type= "text" placeholder="Type here"/>
-}
-
-const Btn = () =>{
-  const text = "Log in";
-  
-  return  <button>{text}</button>
-}
-
-const App = () =>{
-  return (
-    <div>
-    <Header/>
-    <Field/>
-    <Btn/>
-    <div>sasd</div>
-    </div>
-    
-  )
-}
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <App/>
-);
-
+import App from './App.js'
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   App
+// );
+ReactDOM.render(<App />, document.getElementById('root'));
 // ReactDOM.render(<App/>, document.getElementById('root'));
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
