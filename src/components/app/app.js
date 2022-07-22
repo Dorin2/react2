044c43389e6fa2eadfbed934f7828ad6c93f61ab
Dorin <dorin.ghetus98@gmail.com>
2022-07-22 22:06:57 +0300

    c13

---
+++ src/components/app/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// import AppHeader from '../app-header';
+
+const App = () =>{
+    return(
+        // <AppHeader/>
+        <div>
+            Dorin
+        </div>
+    )
+    
+}
+
+export default App;
